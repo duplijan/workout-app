cd170a10eb983730e36072d23d8e1d37eccd81d1
Jan Duplinszki <duplinszkijan@gmail.com>
2019-01-10 19:35:21 +0000

    Update rootReducer.js

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -1,13 +1,11 @@
 import { combineReducers } from "redux";
 import authReducer from "./authReducer";
-import workoutReducer from "./workoutReducer";
 //connect data from the database to the state
 import { firestoreReducer } from "redux-firestore";
 import { firebaseReducer } from "react-redux-firebase";
 
 const rootReducer = combineReducers({
   auth: authReducer,
-  workout: workoutReducer,
   firestore: firestoreReducer,
   firebaseAuth: firebaseReducer
 });
